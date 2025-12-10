dfd14a79d79d56016d5a9b8646c7e27634872523
Maël Barbe <bmael4072@gmail.com>
2025-12-10 13:11:16 +0100

    correction des erreur de nom pour le fichier message au lieu de massage et correction du fichier react.vue

--- src/React.vue
+++ src/React.vue
@@ -1,79 +1,28 @@
 <script setup>
-import { ref, reactive } from "vue"
-
-const variable = 0
-const refVariable = ref(10)
-const reactiveVariable = reactive({ data: 20 })
-
-const myTimer = ref(null);
-function startTimer() {
-  myTimer.value = setInterval(() => {
-    refVariable.value += 1;
-    reactiveVariable.data += 1;
-  }, 1000)
+import { ref, reactive } from 'vue'
+const variable = 0;
+const refVariable = ref(0);
+const reactiveVariable = reactive({value: 0});
+let myTimer;
+function start() {
+    myTimer = setInterval(() => {
+        refVariable.value++;
+        reactiveVariable.value++;
+    }, 1000);
 }
-
-function stopTimer() {
-  if (myTimer.value) {
-    clearInterval(myTimer.value);
-    myTimer.value = null;
-  }
+function stop() {
+    clearInterval(myTimer);
 }
-
-
-
-
-
-
-
-
-
-
 </script>
-
-<template>
-
-  <span>{{ variable }}</span>
-  <br>
-  <span>{{ refVariable }}</span>
-  <br>
-  <span>{{ reactiveVariable.data }}</span>
-  <br>
-  <button @click="startTimer">START</button>
-  <br>
-  <button @click="stopTimer">STOP</button>
-</template>
-<script setup>
-    import { ref, reactive } from 'vue'
-
-    const variable = 0;
-    const refVariable = ref(0);
-    const reactiveVariable = reactive({value: 0});
-    let myTimer;
-
-    function start() {
-        myTimer = setInterval(() => {
-            refVariable.value++;
-            reactiveVariable.value++;
-        }, 9);
-    }
-
-    function stop() {
-        clearInterval(myTimer);
-    }
-
-
-
-</script>
-
 <template>
     <span>{{ variable }}</span>
+    <br>
     <span>{{ refVariable }}</span>
+    <br>
     <span>{{ reactiveVariable.value }}</span>
-
+    <br>
     <button @click="start()">Start</button>
+    <br>
     <button @click="stop()">Stop</button>
-
 </template>
-
 <style scoped></style>
